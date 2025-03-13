3a53ef54da5df5a672758d944a0a6e5345201026
Maksim-Kostenko <kostenckomaks1@gmail.com>
2025-03-13 23:06:31 +0400

    new version

--- main.py
+++ main.py
@@ -1,46 +1,58 @@
-from random import randint
-import time
+class Person:
 
-def game():
+    def __init__(self):
+        self.__name = None
 
-    name_first_player = input('Введите имя первого игрока: ')
-    name_second_player = input('Введите имя второго игрока: ')
+    @property
+    def username(self):
+        return self.__name
 
-    #Добавить проверку имен, что бы они были разные
-
-    print("Выберите способ определение того, кто начнет игру первым: ")
-    print("1- Я сделаю ход первым;"
-          "2- Соперник сделает ход первым;"
-          "3- Выбрать рандомно.")
-    decision_first_player = f'{name_first_player} выберите вариант от 1 до 3: '
-    decision_second_player = f'{name_second_player} выберите вариант от 1 до 3: '
-
-    first_move_player = None
-    winner_game = None
-
-    if (decision_first_player == 1 and decision_second_player == 2) or (decision_first_player == 2 and decision_second_player == 1):
-        if decision_first_player == 1:
-            print(f'{name_first_player} делает первый ход.')
-            first_move_player = 1
-        else:
-            print(f'{name_second_player} делает первый ход.')
-            first_move_player = 2
-    else:
-        print(f'Выбор игрока произойдет рандомно...'
-              f'Происходит выбор подождите 2 секунды...')
-        randint_first_player, randint_second_player = randint(1, 100), randint(1, 100)
-        time.sleep(2)
-        if randint_first_player < randint_second_player:
-            first_move_player = 2
-            print(f'{name_second_player} делает первый ход.')
-        else:
-            first_move_player = 1
-            print(f'{name_first_player} делает первый ход.')
-
-        game_map = [['-','-','-'], ['-','-','-'], ['-','-','-']]
-        for string in game_map:
-            for s in string:
-                print(s)
+    @username.setter
+    def username(self, value):
+        if not value:
+            raise ValueError('Имя не может быть пустым!')
+        self.__name = value
 
 
-game()
+def register_players():
+
+    def get_player_name(player_number):
+        return input(f'Введите имя игрока {player_number}: ')
+
+    player_1 = Person()
+    player_2 = Person()
+
+    while not player_1.username:
+        try:
+            player_1.username = get_player_name(1)
+        except ValueError as e:
+            print(e)
+
+    while not player_2.username:
+        try:
+            player_2.username = get_player_name(2)
+        except ValueError as e:
+            print(e)
+
+    while player_1.username == player_2.username:
+        print('Имена игроков не должны быть одинаковыми!')
+        try:
+            num_player = int(input('Введите номер игрока, чье имя хотите изменить (1 или 2): '))
+            if num_player == 1:
+                player_1.username = get_player_name(1)
+            elif num_player == 2:
+                player_2.username = get_player_name(2)
+            else:
+                print("Некорректный номер игрока. Пожалуйста, введите 1 или 2.")
+        except ValueError:
+            print("Ошибка: введите число (1 или 2).")
+
+    print(f"Игрок 1: {player_1.username}")
+    print(f"Игрок 2: {player_2.username}")
+
+
+def choose_first_player():
+    print('Выбирите игрока, кто будет делать первый ход:')
+
+
+register_players()
