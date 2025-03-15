fe2c51292d895e4057ec74bb6223b8d609d01095
Maksim-Kostenko <kostenckomaks1@gmail.com>
2025-03-15 19:19:28 +0400

    Итоговая версия

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import random
 
 class Person:
     __next_id = 1
-    instances = list()
 
     def __new__(cls, *args, **kwargs):
         instance = super().__new__(cls)
@@ -12,7 +11,6 @@ class Person:
 
     def __init__(self):
         self.__name = None
-        Person.instances.append(self)
 
     @property
     def username(self):
@@ -24,11 +22,10 @@ class Person:
             raise ValueError('Имя не может быть пустым!')
         elif not value.isalpha():
             raise ValueError('Имя может содержать только буквы!')
+        elif len(value) > 11:
+            raise ValueError('Слишком длинное имя, максимальнав длина имени 10 букв!')
         self.__name = value.title()
 
-    @classmethod
-    def get_instances(cls):
-        return cls.instances
 
 player_1 = Person()
 player_2 = Person()
@@ -37,22 +34,26 @@ player_2 = Person()
 
 
 def register_players():
+    """Функция для регистрации игроков."""
 
     def get_player_name(player_id):
+        """Запрашивает имя игрока."""
         return input(f'Введите имя игрока {player_id}: ')
 
+    def set_player_username(player):
+        """
+        Функция реализует присваивание имени игрока к экземпляру класса переданного в качестве аргумента
+        :param player: экземпляр класса Person
+        :return: функция ничего не возвращает (None)
+        """
+        while not player.username:
+            try:
+                player.username = get_player_name(player.id)
+            except ValueError as e:
+                print(e)
 
-    while not player_1.username:
-        try:
-            player_1.username = get_player_name(player_1.id)
-        except ValueError as e:
-            print(e)
-
-    while not player_2.username:
-        try:
-            player_2.username = get_player_name(player_2.id)
-        except ValueError as e:
-            print(e)
+    set_player_username(player_1)
+    set_player_username(player_2)
 
     while player_1.username == player_2.username:
         print('Имена игроков не должны быть одинаковыми!')
@@ -72,32 +73,41 @@ def register_players():
 
 
 def selection_first_player():
-    """ Доделать нормально, сделать цикл"""
+    """ Функция выбора первого игрока(хотел реализовать выбор первого игрока по голосованию, но не хватило времени на это,
+    оставил это на потом, при этом задание этого не требует)"""
     option = 'Выбирите способ определения игрока, который будет делать первый ход:\n1-Выбрать игрока случайным образом.'
-    print(option)
-    try:
-        selected_option = int(input('Введите номер способа: '))
-        if selected_option == 1:
-            first_player = random.choice([player_1, player_2])
-            second_player = player_2 if first_player == player_1 else player_1
-            print(f'{first_player.username} ходит первым!')
-            return first_player, second_player
-        else:
-            raise ValueError(f'Некорректный выбор!'
-                             f'Введите число из представленных способов'
-                             f'{option}')
-    except ValueError as e:
-        print(e)
+    while True:
+        print(option)
+        try:
+            selected_option = int(input('Введите номер способа: '))
+            if selected_option == 1:
+                first_player = random.choice([player_1, player_2])
+                second_player = player_2 if first_player == player_1 else player_1
+                print(f'{first_player.username} ходит первым!')
+                return first_player, second_player
+            else:
+                raise ValueError(f'Некорректный выбор!'
+                                 f'Введите число из представленных способов'
+                                 f'{option}')
+        except ValueError as e:
+            print(e)
 
 
 
 def draw_board(board):
+    """ Выводит изображения игрового поля в терминал"""
     print("-" * 13)
     for i in range(3):
         print("|", board[0 + i * 3], "|", board[1 + i * 3], "|", board[2 + i * 3], "|")
         print("-" * 13)
 
+def reset_board(board):
+    """Сбрасывает игровое поле для новой игры."""
+    for i in range(9):
+        board[i] = i + 1
+
 def take_position(player_token, board, player):
+    """Проверяет и устанаввливает Х или О на игровом поле"""
     valid = False
     while not valid:
         player_answer = input(player + ",куда поставим " + player_token + "? ")
@@ -116,6 +126,7 @@ def take_position(player_token, board, player):
             print("Некорректный ввод. Введите число от 1 до 9.")
 
 def check_win(board):
+    """Проверяет наличие выйгрышных комбинаций на игровом поле"""
     win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
     for each in win_coord:
         if board[each[0]] == board[each[1]] == board[each[2]]:
@@ -123,6 +134,7 @@ def check_win(board):
     return False
 
 def game(board, first_player, second_player):
+    """Цикл реализующий процесс игры"""
     counter = 0
     win = False
     while not win:
@@ -133,16 +145,14 @@ def game(board, first_player, second_player):
             take_position("O", board, second_player.username)
         counter += 1
 
-        tmp = check_win(board)
-        if tmp:
-            if tmp == "X":
+        winner_token = check_win(board)
+        if winner_token:
+            if winner_token == "X":
                 print(first_player.username, "выиграл!")
             else:
                 print(second_player.username, "выиграл!")
             win = True
             break
-        else:
-            print(first_player.username, "выиграл!")
 
         if counter == 9:
             print("Ничья!.")
@@ -152,14 +162,27 @@ def game(board, first_player, second_player):
 
 
 def main():
-    print('Добро пожаловать в игру "Крестики-нолики!"\nДавайте познакомимся!')
-    register_players()
-    print('Приступим к выбору игрока, который будет делать первый ход!')
-    first_player, second_player = selection_first_player()
-    print('Приступим к игре!')
-    board = list(range(1, 10))
-    game(board, first_player, second_player)
+    try:
+        """Основная функция"""
+        print('Добро пожаловать в игру "Крестики-нолики!"\nДавайте познакомимся!')
+        register_players()
+        print('Приступим к выбору игрока, который будет делать первый ход!')
+        first_player, second_player = selection_first_player()
+        print('Приступим к игре!')
+        board = list(range(1, 10))
+        game(board, first_player, second_player)
 
+        while True:
+            play_again = input("Хотите сыграть еще раз? (да/нет): ").lower()
+            if play_again == "да":
+                reset_board(board)
+                first_player, second_player = selection_first_player()
+                game(board, first_player, second_player)
+            else:
+                print("Спасибо за игру! До свидания!")
+                break
+    except KeyboardInterrupt:
+        print('Игра завершена принудительно!')
 
 if __name__ == '__main__':
     main()
